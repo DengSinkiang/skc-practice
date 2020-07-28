42319d4020cc22b2432c0cd6ccdb3e69f57aa5ee
DengSinkiang <dengsinkiang@gmail.com>
2020-07-28 16:59:52 +0800

    dxj skc

---
+++ src/test/java/com/dxj/skc/SkcTest.java
@@ -0,0 +1,21 @@
+package com.dxj.skc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Description:
+ * @Author: dengxj29231
+ * @Date: 2020/7/28 16:52
+ * @CopyRight: 2020 hundsun all rights reserved.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SkcTest {
+    @Test
+    public void testLoad() {
+        System.out.println("hello skc");
+    }
+}
