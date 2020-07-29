093adbc572baf9bbcc4f0893bc840233d8911334
DengSinkiang <dengsinkiang@gmail.com>
2020-07-29 16:56:07 +0800

    dxj skc

--- src/main/java/com/dxj/skc/curator/controller/CuratorLockController.java
+++ src/main/java/com/dxj/skc/curator/controller/CuratorLockController.java
@@ -26,7 +26,7 @@ public class CuratorLockController {
         boolean flag;
         distributedLockByCurator.acquireDistributedLock(PATH);
         try {
-            Thread.sleep(10000);
+            Thread.sleep(20000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -39,7 +39,7 @@ public class CuratorLockController {
         boolean flag;
         distributedLockByCurator.acquireDistributedLock(PATH);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
