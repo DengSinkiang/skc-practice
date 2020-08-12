b9479a04b7837dd52dc454c21a6f087ed9b80ffb
DengSinkiang <dengsinkiang@gmail.com>
2020-08-12 20:41:22 +0800

    dxj skc

--- src/test/java/com/dxj/skc/SkcTest.java
+++ src/test/java/com/dxj/skc/SkcTest.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,8 +24,8 @@ import java.util.List;
  * @Date: 2020/7/28 16:52
  * @CopyRight: 2020 sk-admin all rights reserved.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SkcApplication.class)
 public class SkcTest {
     @Autowired
     private AmqpTemplate amqpTemplate;
